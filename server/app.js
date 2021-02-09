dfdf88dc1f6b106adcaccd5ea60c11009f3394bf
mahfuz0498 <mahfuzhasan0498@gmail.com>
2021-02-09 10:28:21 +0530

    task2

--- server/app.js
+++ server/app.js
@@ -1,53 +1,81 @@
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const PORT = 5000;
-require("./model")
+require("./model");
 const File = mongoose.model("file");
 const router = express.Router();
 
 const storage = multer.diskStorage({
-   destination: "./public/",
-   filename: function(req, file, cb){
-      cb(null,"IMAGE-" + Date.now() + path.extname(file.originalname));
-   }
+  destination: "./public/",
+  filename: function(req, file, cb) {
+    cb(null, "IMAGE-" + Date.now() + path.extname(file.originalname));
+  }
 });
 
 const upload = multer({
-   storage: storage,
-   limits:{fileSize: 1000000},
+  storage: storage,
+  limits: { fileSize: 1000000 }
 }).single("myfile");
 
-const obj =(req,res) => {
-   console.log("nigga")
-   upload(req, res, () => {
-      console.log("Request ---", req.body);
-      console.log("Request file ---", req.file);//Here you get file.
-      const file = new File();
-      file.meta_data = req.file;
-      file.save().then(()=>{
-      res.send({message:"uploaded successfully"})
-      })
-      /*Now do where ever you want to do*/
-   });
-}
+const obj = (req, res) => {
+  console.log("nigga");
+  upload(req, res, () => {
+    console.log("Request ---", req.body);
+    console.log("Request file ---", req.file); //Here you get file.
+    const file = new File();
+    file.meta_data = req.file;
+    file.save().then(() => {
+      res.send({ message: "uploaded successfully" });
+    });
+    /*Now do where ever you want to do*/
+  });
+};
 
 router.post("/upload", obj);
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+  );
+
+  next();
+});
 app.use(router);
 
-app.get("/",(req,res)=>{
-   return res.send("<p>hello!</p>");
+app.get("/", (req, res) => {
+  return res.send("<p>hello!</p>");
 });
 
-mongoose.connect("mongodb://localhost/file-upload",{
-   useUnifiedTopology: true,
-   useNewUrlParser: true,
-   useCreateIndex: true,
-}).then(()=>{console.log("DB is connected")})
+mongoose
+  // type our own mongodb link here;
+  .connect(
+    "mongodb+srv://mahfuz:vbUWRvDgHzSxaYCw@cluster0.vue3q.mongodb.net/backend1?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
+  )
+  .then(() => {
+    console.log("DB is connected");
+  });
 
-app.listen(PORT,()=>{
-   console.log("\u{1F525}\u{1F680} app listen on port",PORT,"\u{1F525}\u{1F680}")
+app.listen(PORT, () => {
+  console.log(
+    "\u{1F525}\u{1F680} app listen on port",
+    PORT,
+    "\u{1F525}\u{1F680}"
+  );
 });
--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends React.Component {
-
-  state ={
+  state = {
     file: null
   };
-  onFormSubmit = (e) => {
-      e.preventDefault();
-      const formData = new FormData();
-      formData.append('myfile',this.state.file);
-      const config = {
-          headers: {
-              'content-type': 'multipart/form-data'
-          }
-      };
-      axios.post("http://localhost:5000/upload", formData, config)
-          .then((response) => {
-              alert("The file is successfully uploaded");
-          }).catch((error) => {
-              alert(error);
+  onFormSubmit = e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("myfile", this.state.file);
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+    axios
+      .post("http://localhost:5000/upload", formData, config)
+      .then(response => {
+        alert("The file is successfully uploaded");
+      })
+      .catch(error => {
+        alert(error);
       });
-  }
+  };
 
-  onChange = (e) => {
-      this.setState({file:e.target.files});
-  }
+  onChange = e => {
+    this.setState({ file: e.target.files });
+  };
 
   render() {
-      return (
-          <form onSubmit={this.onFormSubmit}>
-              <h1>File Upload</h1>
-              <input type="file" className="custom-file-input" name="myImage" onChange= {this.onChange} />
-              <button className="upload-button" type="submit">Upload to DB</button>
-          </form>
-      )
+    return (
+      <form className="form-upload" onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input
+          type="file"
+          className="custom-file-input"
+          name="myImage"
+          onChange={this.onChange}
+        />
+        <button className="upload-button" type="submit">
+          Upload to DB
+        </button>
+      </form>
+    );
   }
 }
 
